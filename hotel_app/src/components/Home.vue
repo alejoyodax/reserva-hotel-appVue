4c9a6cc9b0083b6c6fd06f8f6e93dd0dc23f8c1c
Alejandro Silva <alejoyodax@gmail.com>
2020-12-15 00:30:04 -0500

    Primera version funcional

--- hotel_app/src/components/Home.vue
+++ hotel_app/src/components/Home.vue
@@ -4,35 +4,61 @@
     
     <form id="formulario">
       <label class="lform" for="fnombre">Usuario: </label>
-      <input  class="iform" type="text" id="fnombre" name="fnombre"></br>
+      <input  v-model="user" class="iform" type="text" id="fnombre" name="fnombre"></br>
       
       <label class="lform" for="fapellido">Contraseña: </label>
-      <input class="iform" type="password" id="fcontraseña" name="fcontraseña"></br>
+      <input v-model="pass" class="iform" type="password" id="fcontraseña" name="fcontraseña"></br>
 
     </form>
 
-    <input type="button" id="boton-aceptar" value="Aceptar">
+    <input v-on:click="hacerLogin" type="button" id="boton-aceptar" value="Aceptar">
 
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data: function () {
     return {
-      msg: 'Componente ingreso de usuarios'
-    }
+      user: null,
+      pass: null
+    };
+  },
+
+  methods: {
+    hacerLogin: function () {
+      this.user
+      var datosJSon = { 
+        "username": this.user, 
+        "contrasena": this.pass 
+        };
+      console.log(datosJSon);
+
+      axios
+        .post("http://127.0.0.1:8000/cliente/auth" , datosJSon)
+        .then((respuesta) => {
+          if (respuesta.data.Autenticado) {
+            console.log("AUTENTICADO CORRECTAMENTE");
+            alert("AUTENTICADO CORRECTAMENTE");
+          }else{
+            console.log("NO AUTENTICADO");
+          }
+        }).catch(error => {
+        console.log(error)
+        this.errored = true
+    });
   }
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-#caja-formulario{
+#caja-formulario {
   margin: 80px 280px 80px;
-  background-color:white;
+  background-color: white;
   height: 300px;
 
   border: solid;
@@ -40,11 +66,11 @@ export default {
   border-radius: 5px;
 }
 
-h1{
+h1 {
   padding-top: 0px;
 }
 
-label{
+label {
   display: inline-block;
   width: 100px;
   text-align: left;
@@ -55,12 +81,12 @@ label{
   font-size: 20px;
 }
 
-#formulario input{
+#formulario input {
   height: 23px;
   margin: 0px 0px 20px 0px;
 }
 
-#boton-aceptar{
+#boton-aceptar {
   width: 100px;
   height: 33px;
   background-color: white;
@@ -68,15 +94,14 @@ label{
   margin: 0px;
 }
 
-#boton-aceptar:hover{
-  background-color:gray;
+#boton-aceptar:hover {
+  background-color: gray;
   color: white;
   font: bold;
 }
 
-#boton-aceptar:active{
+#boton-aceptar:active {
   background-color: rgb(48, 48, 48);
   font: bold;
 }
-
 </style>
