0021cd1bf4fef0fe5ffd7716d7162cc5cb893e99
Alejandro Silva <alejoyodax@gmail.com>
2020-12-14 20:26:40 -0500

    Funciones añadidas

---
+++ hotel_app/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <h1>Sistemas Reservaciones Online</h1>
+    </div>
+    <div class="main-component">
+      <Home></Home>
+    </div>
+    <div class="footer">
+      <h2>MinTic - Ciclo III - 2020</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0 0 0 0;
+}
+
+.header {
+  margin: 0%;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+
+  background-color: #283747;
+  color: #e5e7e9;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  
+}
+.header h1 {
+  width: 100%;
+  text-align: center;
+}
+
+.footer {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #283747;
+  color: #e5e7e9;
+}
+.footer h2 {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+
+</style>
---
+++ hotel_app/src/components/Home.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="caja-formulario">
+    <h1>Iniciar sesión</h1>
+    
+    <form id="formulario">
+      <label class="lform" for="fnombre">Usuario: </label>
+      <input  class="iform" type="text" id="fnombre" name="fnombre"></br>
+      
+      <label class="lform" for="fapellido">Contraseña: </label>
+      <input class="iform" type="password" id="fcontraseña" name="fcontraseña"></br>
+
+    </form>
+
+    <input type="button" id="boton-aceptar" value="Aceptar">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Componente ingreso de usuarios'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#caja-formulario{
+  margin: 80px 280px 80px;
+  background-color:white;
+  height: 300px;
+
+  border: solid;
+  border-color: darkslategray;
+  border-radius: 5px;
+}
+
+h1{
+  padding-top: 0px;
+}
+
+label{
+  display: inline-block;
+  width: 100px;
+  text-align: left;
+  margin-right: 10px;
+}
+
+#formulario {
+  font-size: 20px;
+}
+
+#formulario input{
+  height: 23px;
+  margin: 0px 0px 20px 0px;
+}
+
+#boton-aceptar{
+  width: 100px;
+  height: 33px;
+  background-color: white;
+  font: bold;
+  margin: 0px;
+}
+
+#boton-aceptar:hover{
+  background-color:gray;
+  color: white;
+  font: bold;
+}
+
+#boton-aceptar:active{
+  background-color: rgb(48, 48, 48);
+  font: bold;
+}
+
+</style>
